2e104c1e0fceef0b84f032b7e2f7f7210f969553
Christopher-John Cassar <46441540+cjmcassar@users.noreply.github.com>
2021-04-16 12:20:43 +0100

    Added the handleChange function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+  // const [lName, setLName] = useState("");
+
+  // function updateFName(event) {
+  //   const firstName = event.target.value;
+  //   setFName(firstName);
+  // }
+
+  // function updateLName(event) {
+  //   const lastName = event.target.value;
+  //   setLName(lastName);
+  // }
+
+  function handleCHange(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          onChange={handleCHange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleCHange}
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
